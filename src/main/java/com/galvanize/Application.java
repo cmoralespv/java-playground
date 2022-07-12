7351de9b917abf69975424930dd33df3830d730c
Carlos Morales <65197354+cmoralespv@users.noreply.github.com>
2022-07-12 11:30:23 -0700

    Read Command Line Arguments Assignment

--- src/main/java/com/galvanize/Application.java
+++ src/main/java/com/galvanize/Application.java
@@ -4,8 +4,14 @@ public class Application {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
+        if (args.length == 0) {
+            System.out.println("Please specify a name and email");
+            System.exit(1);
+        } else if (args.length == 1) {
+            System.out.println(String.format("Please specify an email for %s", args[0]));
+            System.exit(1);
+        }
 
+        System.out.println(String.format("%s <%s>", args[0], args[1]));
     }
-
 }
