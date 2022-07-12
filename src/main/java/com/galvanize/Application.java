c05a488934888c30cece729b00ef5cde04b3c49e
Carlos Morales <65197354+cmoralespv@users.noreply.github.com>
2022-07-12 16:16:05 -0700

    Roman To Decimal Assignment

--- src/main/java/com/galvanize/Application.java
+++ src/main/java/com/galvanize/Application.java
@@ -1,5 +1,7 @@
 package com.galvanize;
 
+import java.util.HashMap;
+
 public class Application {
 
     public static void main(String[] args) {
@@ -11,6 +13,8 @@ public class Application {
         System.out.println(fizzBuzz(5));
         System.out.println(fizzBuzz(15));
         System.out.println(fizzBuzz(2));
+
+        System.out.println(romanToDecimal("MCMXLIV"));
     }
 
     public static String emailFormat(String[] inputs) {
@@ -40,5 +44,40 @@ public class Application {
             result = String.format("%s", input);
         }
        return result;
+    }
+
+    public static Integer romanToDecimal(String input) {
+
+        Integer decimalValue = 0;
+
+        HashMap<Character, Integer> equivalence = new HashMap<Character, Integer>();
+
+        equivalence.put('I', 1);
+        equivalence.put('V', 5);
+        equivalence.put('X', 10);
+        equivalence.put('L', 50);
+        equivalence.put('C', 100);
+        equivalence.put('D', 500);
+        equivalence.put('M', 1000);
+
+        for (int i = 0; i < input.length() - 1; i++) {
+
+            Integer current = equivalence.get(input.charAt(i));
+            Integer next = equivalence.get(input.charAt(i+1));
+
+            if (current < next) {
+                decimalValue += next - current;
+                if ( i + 1 >= input.length() - 1) {
+                    break;
+                } else {
+                    i++;
+                }
+            } else {
+                decimalValue += current;
+            }
+
+        }
+
+        return decimalValue;
     }
 }
